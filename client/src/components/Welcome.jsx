5a16c0f7c27f0c9a4f077232909966296dd0d698
mounishvatti <mounishvatti2002@gmail.com>
2023-11-01 12:47:10 +0530

    updated ui

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,108 @@
+import { AiFillAlipayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from './';
+
+const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
+
 const Welcome = () => {
+    const connectWallet = () => {
+        
+    }
+    const handleSubmit = () => {
+
+    }
     return (
-        <h1>Welcome</h1>
+        <div className="flex w-full justify-center items-center">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+                        Send Crypto <br /> across the world
+                    </h1>
+                    <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+                        Explore the crypto world. Buy and sell cryptocurrencies easily on PandaCoin.
+                    </p>
+                    <button
+                        type="button"
+                        onClick={connectWallet}
+                        className="flex flex-row justify-center items-center my-5 bg-[#929530] p-3 rounded-full cursor-pointer hover:bg-[#bda425]"
+                    >
+                        <p className="text-white text-base font-semibold">Connect Wallet</p>
+                    </button>
+                    <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+                        <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
+                            Reliability
+                        </div>
+                        <div className={companyCommonStyles}>Security</div>
+                        <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
+                            Ethereum
+                        </div>
+                        <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
+                            Web 3.0
+                        </div>
+                        <div className={companyCommonStyles}>Low Fees</div>
+                        <div className={`rounded-br-2xl ${companyCommonStyles}`}>
+                            Blockchain
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+                    <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+                        <div className="flex justify-between flex-col w-full h-full">
+                            <div className="flex justify-between items-start">
+                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-cente">
+                                    <SiEthereum fontSize={23} color="#fff" />
+                                </div>
+                                <BsInfoCircle fontSize={16} color="#fff" />
+                            </div>
+                            <div>
+                                <p className="text-white font-light text-sm">Address</p>
+                                <p className="text-white font-semibold text-lg mt-1">Ethereum</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center yellowgreen-glassmorphism">
+                        <Input placeholder="Address To" name="addressTo" type="text" />
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" />
+                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" />
+                        <Input placeholder="Enter Message" name="message" type="text" />
+
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
+                        
+                        {false ? (
+                            <Loader />
+                        ) : (
+                         <div>
+                            <button
+                            type="button"
+                            onClick={handleSubmit}
+                            className="text-white w-full mt-2 border-[1px] p-2 border-[#747c3d] bg-[#4f5529] hover:bg-[#37381f] rounded-full cursor-pointer"
+                            >
+                                Send now
+                            </button>
+                         </div>
+
+                        )}
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
     );
 }
 
