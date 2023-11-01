f9fe3ae0d45abc37cb1eb66aab4c020c0bc93d9a
mounishvatti <mounishvatti2002@gmail.com>
2023-11-01 11:39:30 +0530

    added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import { Footer, Loader, Navbar, Services, Transactions, Welcome } from './components'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
+    </div>
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
   )
 }
 
